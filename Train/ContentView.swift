21827d6d5fe42982f806cc5f502934160632a1c1
sadbutcutee <badcode3@yandex.ru>
2026-06-12 12:41:56 +0300

    Initial Commit

---
+++ Train/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Train
+//
+//  Created by Александр Гладков on 12.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Train/TrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainApp.swift
+//  Train
+//
+//  Created by Александр Гладков on 12.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct TrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
